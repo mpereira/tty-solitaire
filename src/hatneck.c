828848425c8f4818e4ecc53434abc0a5ec2ac095
Murilo Soares Pereira <murilo.soares3@gmail.com>
2010-03-28 20:42:05 -0300

    Created hatneck.c.

---
+++ src/hatneck.c
@@ -0,0 +1,14 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  hatneck.c
+ *
+ *    Description:  hatneck is a ncurses-based solitaire game
+ *
+ *        Created:  03/28/2010 04:58:26 PM
+ *       Compiler:  gcc
+ *
+ *         Author:  Murilo Soares Pereira
+ *
+ * =============================================================================
+ */
