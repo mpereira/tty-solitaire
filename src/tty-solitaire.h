06e292bc074fdd7e2da5d069afad5208ac92fc5e
Murilo Soares Pereira <murilo.soares3@gmail.com>
2010-03-28 21:12:53 -0300

    Added the header file to tty-solitaire.c.

---
+++ src/tty-solitaire.h
@@ -0,0 +1,14 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  tty-solitaire.h
+ *
+ *    Description:  ncurses-based solitaire game
+ *
+ *        Created:  03/28/2010 08:59:52 PM
+ *       Compiler:  gcc
+ *
+ *         Author:  Murilo Soares Pereira
+ *
+ * =============================================================================
+ */
