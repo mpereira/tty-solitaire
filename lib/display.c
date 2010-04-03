042b45f36a0bef75ec74313a977340b0203f457b
Murilo Soares Pereira <murilo.soares3@gmail.com>
2010-04-03 15:24:10 -0300

    Forgot switch's breaks.

--- lib/display.c
+++ lib/display.c
@@ -10,10 +10,10 @@ char *card_suit(enum suit suit) {
   card_suit = malloc(5 * sizeof(card_suit));
 
   switch(suit) {
-    case DIAMONDS: strcpy(card_suit, DIAMONDS_SYMBOL);
-    case SPADES:   strcpy(card_suit, SPADES_SYMBOL);
-    case HEARTS:   strcpy(card_suit, HEARTS_SYMBOL);
-    case CLUBS:    strcpy(card_suit, CLUBS_SYMBOL);
+    case DIAMONDS: strcpy(card_suit, DIAMONDS_SYMBOL); break;
+    case SPADES:   strcpy(card_suit, SPADES_SYMBOL);   break;
+    case HEARTS:   strcpy(card_suit, HEARTS_SYMBOL);   break;
+    case CLUBS:    strcpy(card_suit, CLUBS_SYMBOL);    break;
     default:       strcpy(card_suit, "?");
   }
 
