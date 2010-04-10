121e49e2b745ceb776e358b64b5e2c89a2457be6
Murilo Soares Pereira <murilo.soares3@gmail.com>
2010-04-10 13:59:04 -0300

    Inform what stack we're printing.

--- debug/deck_debug.c
+++ debug/deck_debug.c
@@ -3,21 +3,47 @@
 #include "deck_debug.h"
 
 void print_deck(struct deck *deck) {
+  puts("stock");
   print_stack(deck->stock);
+  puts("");
+  puts("waste_pile");
   print_stack(deck->waste_pile);
+  puts("");
 
+  puts("foundation_0");
   print_stack(deck->foundation_0);
+  puts("");
+  puts("foundation_1");
   print_stack(deck->foundation_1);
+  puts("");
+  puts("foundation_2");
   print_stack(deck->foundation_2);
+  puts("");
+  puts("foundation_3");
   print_stack(deck->foundation_3);
+  puts("");
 
+  puts("maneuvre_0");
   print_stack(deck->maneuvre_0);
+  puts("");
+  puts("maneuvre_1");
   print_stack(deck->maneuvre_1);
+  puts("");
+  puts("maneuvre_2");
   print_stack(deck->maneuvre_2);
+  puts("");
+  puts("maneuvre_3");
   print_stack(deck->maneuvre_3);
+  puts("");
+  puts("maneuvre_4");
   print_stack(deck->maneuvre_4);
+  puts("");
+  puts("maneuvre_5");
   print_stack(deck->maneuvre_5);
+  puts("");
+  puts("maneuvre_6");
   print_stack(deck->maneuvre_6);
+  puts("");
 
   return;
 }
