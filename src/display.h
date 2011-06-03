b5811c5d6d879165a73733ccbb2d75455ab68c9d
Murilo Pereira <murilo@murilopereira.com>
2011-06-03 00:18:17 -0300

    Adding some function declarations and removing wtf.

--- src/display.h
+++ src/display.h
@@ -6,18 +6,17 @@
 #include "deck.h"
 #include "cursor.h"
 
-#define EMPTY_STACKS_NUMBER 13
-
 #define BLACK_ON_WHITE 1
 #define RED_ON_WHITE   2
 #define WHITE_ON_BLUE  3
 #define WHITE_ON_GREEN 4
 
-void erase_stack(struct stack *);
 void draw_card(struct card *);
 void draw_stack(struct stack *);
 void draw_deck(struct deck *);
 void draw_cursor(struct cursor *);
+void erase_card(struct card *);
+void erase_stack(struct stack *);
 void erase_cursor(struct cursor *);
 
 #endif
