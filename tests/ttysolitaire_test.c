fac0750a2cfe199d49e1752cca9d849c57ef615c
Murilo Pereira <murilo@murilopereira.com>
2013-05-04 16:40:33 -0400

    Fix test run.

--- tests/ttysolitaire_test.c
+++ tests/ttysolitaire_test.c
@@ -1,9 +1,13 @@
 #include "ttysolitaire_test.h"
+#include "../src/game.h"
 
 const char *program_name;
+struct game game;
 
 int main(int argc, const char *argv[]) {
   program_name = argv[0];
+
+  (void) argc;
 
   test_card();
   test_cursor();
