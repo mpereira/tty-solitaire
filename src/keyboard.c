324b9872abe4b352bfc476f1dffc5975a023bd9b
Murilo Pereira <murilo@murilopereira.com>
2017-01-07 20:37:08 -0200

    Only maneuvre stacks are marked.

--- src/keyboard.c
+++ src/keyboard.c
@@ -160,7 +160,7 @@ static void handle_card_movement(struct cursor *cursor) {
           erase_cursor(cursor);
           cursor->y--;
         }
-        if (maneuvre_stack(*origin)) {
+        if (marked_cards_count(*origin) > 0 && maneuvre_stack(*origin)) {
           erase_stack(*origin);
           unmark_cards(*origin);
           draw_stack(*origin);
